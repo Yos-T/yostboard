dd5d988322096ad010dc656fd03a93ddca3e7489
Yos-T <yos.t.zz@gmail.com>
2017-09-08 13:41:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"
+var server_port = 8080
+var server_ip_address = "0.0.0.0"
 var websocket_port = 8000
 
 var server = express();
